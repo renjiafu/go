a76e4658f5628777d52ffa3d0a830a132d5d593b
rene <154650177@qq.com>
2022-09-02 00:03:55 +0800

    golang 切片

--- src/slices/slice.go
+++ src/slices/slice.go
@@ -2,60 +2,26 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
 func main() {
 
-	primes := [6]int{2, 3, 1, 5, 8, 0}
-
-	// 切片左闭右开
-	var s []int = primes[1:4]
-	fmt.Println(s)
-
-	names := [4]string{
-		"John",
-		"Paul",
-		"George",
-		"Ringo",
-	}
-	fmt.Println(names)
-
-	a := names[0:2]
-	b := names[1:3]
-	fmt.Println(a, b)
-
-	b[0] = "xxx"
-	fmt.Println(a, b)
-	fmt.Println(names)
-
-	str := []int{2, 3, 5, 7, 10}
-	printSlice(str)
-
-	s = s[:0]
-	printSlice(s)
-
-	s = s[:4]
-	printSlice(s)
-
-	s = s[2:]
-	printSlice(s)
-
-	//nil slices
 	var s1 []int
-	fmt.Println(s1, len(s1), cap(s1))
+	fmt.Println("切片初始化 --> ", "s1", s1, "type", reflect.TypeOf(s1), "len", len(s1), "cap", cap(s1))
 
 	s2 := make([]int, 5)
-	printSlice(s2)
+	fmt.Println("切片初始化 --> ", "s2", s2, "type", reflect.TypeOf(s2), "len", len(s2), "cap", cap(s2))
 
 	s3 := make([]int, 0, 5)
-	printSlice(s3)
+	fmt.Println("切片初始化 --> ", "s3", s3, "type", reflect.TypeOf(s3), "len", len(s3), "cap", cap(s3))
 
 	s4 := s3[:2]
-	printSlice(s4)
+	fmt.Println("切片截取 --> ", "s4", s4, "type", reflect.TypeOf(s4), "len", len(s4), "cap", cap(s4))
 
 	s5 := s4[2:5]
-	printSlice(s5)
+	fmt.Println("切片截取 --> ", "s5", s5, "type", reflect.TypeOf(s5), "len", len(s5), "cap", cap(s5))
 
 	board := [][]string{
 		[]string{"_", "_", "_"},
@@ -70,20 +36,15 @@ func main() {
 	board[0][2] = "X"
 
 	for i := 0; i < len(board); i++ {
+		fmt.Println("type", reflect.TypeOf(board))
 		fmt.Printf("%s \n", strings.Join(board[i], " "))
 	}
 
 	var s6 []int
-	printSlice(s6)
+	fmt.Println("切片初始化 --> ", "s6", s6, "type", reflect.TypeOf(s6), "len", len(s6), "cap", cap(s6))
 
 	s6 = append(s6, 1)
-	printSlice(s6)
 
 	s6 = append(s6, 2, 3, 4)
-	printSlice(s6)
-
-}
-
-func printSlice(s []int) {
-	fmt.Printf("len = %d, cap = %d , %v \n", len(s), cap(s), s)
+	fmt.Println("s6 -->", s6)
 }
