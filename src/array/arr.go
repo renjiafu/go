9f967743d583d125d9feb60bf3c569272d5e43fb
rene <154650177@qq.com>
2022-09-01 23:36:40 +0800

    golang 数组初始化

--- src/array/arr.go
+++ src/array/arr.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0],a[1])
-	fmt.Println(a)
+func main() {
 
-	primes := [8]int{2,3,4,6,8,9,0,10}
-	fmt.Println(primes)
+	var a1 [2]string
+	a1[0] = "Hello"
+	a1[1] = "World"
+	fmt.Println("数组不带元素初始化 --> ", "a1", a1)
+
+	a2 := [8]int{2, 3, 4, 6, 8, 9, 0, 10}
+	fmt.Println("数组带元素初始化 --> ", "a2", a2)
 }
